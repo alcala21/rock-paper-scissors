5c18921de2a3f6d3dbc88acf7f26be6e652cb178
Carlos Alcala <alcala21@gmail.com>
2020-08-27 12:04:54 -0500

    Stage 5 done. Project completed.

--- game.py
+++ game.py
@@ -3,32 +3,30 @@ import random
 
 class Rps:
     def __init__(self):
-        self.beat = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
+        self.beat = {}
+        self.options = ['rock', 'paper', 'scissors']
         self.scores = {'draw': 50, 'win': 100, 'lose': 0}
         self.ratings = {}
         self.player = ""
-        self.load_ratings()
 
     def play(self):
-        self.player = input('Enter your name: ')
-        print('Hello,', self.player)
-
-        if self.player not in self.ratings:
-            self.ratings[self.player] = 0
+        self.load_ratings()
+        self.select_player()
+        self.select_options()
 
         while True:
             user_input = input()
-            computer = random.choice(list(self.beat.keys()))
+            computer = random.choice(self.options)
 
             if computer == user_input:
                 print(f"There is a draw ({computer})")
                 self.update_rating('draw')
 
-            elif computer == self.beat.get(user_input, ""):
+            elif computer in self.beat.get(user_input, ""):
                 print(f"Sorry, but the computer chose {computer}")
                 self.update_rating('lose')
 
-            elif self.beat[computer] == user_input:
+            elif user_input in self.beat.get(computer, ""):
                 print(f"Well done. The computer chose {computer} and failed.")
                 self.update_rating('win')
 
@@ -50,6 +48,30 @@ class Rps:
             for line in r_file.readlines():
                 k, v = line.strip().split()
                 self.ratings[k] = int(v)
+
+    def select_player(self):
+        self.player = input('Enter your name: ')
+        print('Hello,', self.player)
+
+        if self.player not in self.ratings:
+            self.ratings[self.player] = 0
+
+    def select_options(self):
+        option_str = input()
+        if option_str == "":
+            self.beat = {'rock': ['paper'], 'paper': ['scissors'], 'scissors': ['rock']}
+        else:
+            self.options = option_str.replace(" ", "").split(",")
+            self.make_beat_dict()
+        print("Okay, let's start")
+
+    def make_beat_dict(self):
+        n_reduced = len(self.options) - 1
+        i_beat = n_reduced // 2
+        for word in self.options:
+            w_index = self.options.index(word)
+            new_words = self.options[w_index + 1:] + self.options[:w_index]
+            self.beat[word] = new_words[:i_beat]
 
 
 game = Rps()
